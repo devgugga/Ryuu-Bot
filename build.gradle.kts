c8387867895f56ae8c6360af481595ddb82ec584
devgugga <gustavogomes90104@gmail.com>
2024-11-08 13:13:05 -0300

    🎉 Commit Inicial
    Criação da estrutura da aplicação

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "br.com.ryuu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.2.0")
+    implementation("org.kohsuke:github-api:1.326")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
